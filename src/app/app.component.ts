78e711d73a9265606aef5288954d806bc6cb4eab
zip-fa <61551308+zip-fa@users.noreply.github.com>
2023-11-06 21:28:45 +0200

    bug repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { animate, AnimationBuilder, style } from '@angular/animations';
+import { Component, ElementRef, inject, NgZone, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,5 +11,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng17-async-animations-bug';
+  private readonly animationBuilder = inject(AnimationBuilder);
+  private readonly ngZone = inject(NgZone);
+
+  @ViewChild('el')
+  set el(el: ElementRef<HTMLElement>) {
+    this.playAnimation(el.nativeElement);
+  }
+
+  private playAnimation(el: HTMLElement): void {
+    const animation = this.animationBuilder.build([
+      style({ 'transform': `rotate(0deg)`}), animate(`10000ms cubic-bezier(0.32, 0.64, 0.45, 1)`, style({
+        'transform': `rotate(360deg)`
+      }))
+    ]);
+
+    const anim = animation.create(el);
+
+    this.ngZone.runOutsideAngular(() => {
+      anim.play();
+      anim.setPosition(0.5);
+    });
+  }
 }
